141b9e250af35cf90b7f8464d2206af21099dafb
Paul Sinnett <paul.sinnett@gmail.com>
2018-10-11 14:00:10 +0100

    draw gizmos

--- Assets/Guard.cs
+++ Assets/Guard.cs
@@ -7,9 +7,12 @@ public class Guard : MonoBehaviour
 	void OnDrawGizmos()
 	{
 		Gizmos.color = Color.red;
-		Gizmos.matrix = Matrix4x4.identity;
-		//Gizmos.matrix = transform.localToWorldMatrix;
-		Gizmos.DrawCube(transform.position, Vector3.one * 2.0f);
-		//Gizmos.DrawFrustum(transform.position, 45.0f, 2.0f, 0.5f, 1.0f);
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.DrawFrustum(Vector3.zero, 90.0f, 10.0f, 0.5f, 1.0f);
 	}
+
+    void Update()
+    {
+        
+    }
 }
