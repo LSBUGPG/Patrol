eb1931b2f3e82c01b9dea2d8e55dde07d6e3b225
Paul Sinnett <paul.sinnett@gmail.com>
2021-11-02 11:35:20 +0000

    tidy up

--- Assets/ComponentPackages/Patrol/Scripts/AI.cs
+++ Assets/ComponentPackages/Patrol/Scripts/AI.cs
@@ -6,13 +6,6 @@ public class AI : MonoBehaviour
 {
     public List<Behaviour> states = new List<Behaviour>();
 
-    void SetBehaviour(string behaviourName)
-    {
-        states.ForEach((behaviour) =>
-            behaviour.enabled =
-                (behaviour.GetType().ToString() == behaviourName));
-    }
-
     public void ChangeState(Behaviour newState)
     {
         states.ForEach((state) => state.enabled = (state == newState));
